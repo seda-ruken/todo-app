f2c629d2889bdbc043753bdb26aa351a13e45df9
Ruken Erpolat <72344293+rukenerpolat@users.noreply.github.com>
2023-11-30 16:25:03 +0300

    "dark mode, popup modal button, dropdown button"

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-alert("Hello!")
+/* Dropdown Function */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown 
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+// Dark Mode Function
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+// MODAL PAGE 
+const showModal = document.querySelector(".modal-box"),
+  // overlay = document.querySelector(".overlay"),
+  showBtn = document.querySelector(".btn-add"),
+  applyBtn = document.querySelector(".apply"),
+  cancelBtn = document.querySelector(".cancel");
+
+  // overlay.addEventListener("click"), () => showModal.classList.remove("active");
+  showBtn.addEventListener("click", () => showModal.classList.add("active"));
+  cancelBtn.addEventListener("click", () => showModal.classList.remove("active"));
+  applyBtn.addEventListener("click", () => showModal.classList.remove("active"));
